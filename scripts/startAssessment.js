b7d04d96065a4acc151fcc396b0f657a124406f3
sebastian <sebastianswms@gmail.com>
2023-02-16 13:47:43 -0500

    Add action to the Start button. Stores the user's selected videos in a cookie, then navigates to the assessment.html page.

--- scripts/startAssessment.js
+++ scripts/startAssessment.js
@@ -4,6 +4,8 @@ Class: CS3500
 Date: Spring 2023
 */
 
+import {cookieParse, setCookie} from './cookieManagement.js';
+
 // Before the user begins the assessment, check to make sure the parameters are appropriately set.
 window.validateStartAssessment = function validateStartAssessment(){
     let selection = document.querySelectorAll("input[name=video-entry]:checked"); // Get all selected videos.
@@ -17,4 +19,15 @@ window.validateStartAssessment = function validateStartAssessment(){
         alert("Please set a time between 3 and 90 seconds.")
         return false;
     }
+}
+
+// Store the user's selected video URLs in a cookie and navigate to the assessment.html page.
+window.startAssessment = function startAssessment(){
+    let selection = document.querySelectorAll("input[name=video-entry]:checked"); // Get all selected videos.
+    let values = [];
+    selection.forEach(element => values.push(element.value)); // Put each selected video's URL into a new array.
+
+    setCookie("selection",JSON.stringify(values),5); // Put the array of URLs into a cookie. 5 days until expiry.
+
+    window.location.href = "./assessment.html"; // Navigate to the assessment.html page.
 }
