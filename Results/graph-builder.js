50e009aba350a984516a4462d6cf32ca622b2068
Ali <ali.azimi@wmich.edu>
2023-02-20 18:55:58 -0500

    Added a % on top of each bar graph

--- Results/graph-builder.js
+++ Results/graph-builder.js
@@ -23,7 +23,7 @@ const myChart = new Chart(ctx, {
             this.data.datasets.forEach(function(dataset, i) {
               var meta = chartInstance.controller.getDatasetMeta(i);
               meta.data.forEach(function(bar, index) {
-                var data = dataset.data[index];
+                var data = dataset.data[index] + "%";
                 ctx.fillText(data, bar._model.x, bar._model.y - 5);
               });
             });
