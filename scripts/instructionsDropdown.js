dbd52f1519baabc136d37abccd029968170d58f2
sebastian <sebastianswms@gmail.com>
2023-03-19 17:56:44 -0400

    Code cleanup and refactoring.

---
+++ scripts/instructionsDropdown.js
@@ -0,0 +1,19 @@
+/*
+    Input: The user clicking on the Instruction text to view the instructions.
+
+    Output: All the instruction steps will be displayed for user.
+
+    Remarks: This is the function call when the user needs directions on home page (index.html).
+*/
+var inst_steps = document.getElementById("myDiv");
+var inst_icon = document.getElementById("inst-img");
+inst_steps.style.display = "none";
+window.myFunction = function myFunction() {
+    if (inst_steps.style.display == "none") {
+        inst_steps.style.display = "block";
+        inst_icon.style.transform = 'rotate(180deg)';
+    }else {
+        inst_icon.style.transform = 'rotate(-0deg)';
+        inst_steps.style.display = "none";
+    }
+}
--- scripts/startAssessment.js
+++ scripts/startAssessment.js
@@ -50,25 +50,3 @@ window.startAssessment = function startAssessment(){
 
     window.location.href = "./assessment.html"; // Move on to the assessment.
 }
-
-
-// this function displays/hides the instructions steps, and rotate the img as icon up/down
-/*
-    Input: The user clicking on the Instruction text to view the instructions.
-
-    Output: All the instruction steps will be displayed for user.
-
-    Remarks: This is the function call when the user needs directions on home page (index.html).
-*/
-var inst_steps = document.getElementById("myDiv");
-var inst_icon = document.getElementById("inst-img");
-inst_steps.style.display = "none";
-window.myFunction = function myFunction() {
-    if (inst_steps.style.display == "none") {
-        inst_steps.style.display = "block";
-        inst_icon.style.transform = 'rotate(180deg)';
-    }else {
-        inst_icon.style.transform = 'rotate(-0deg)';
-        inst_steps.style.display = "none";
-    }
-}
