f532470025321489b025ffb9ab47634758516b5a
camillazvincent <camillazvincent@gmail.com>
2023-04-18 04:13:04 -0400

    create index and catalog pages. Update navbar

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+
+        <!-- External stylesheets -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Index</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="references.php">References</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="catalog.php">Catalog</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="api.html">API</a>
+        </li>
+    </ul>
+</nav>
+</body>
+</html>
