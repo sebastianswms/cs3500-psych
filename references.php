b0ae1c3dee73fe1a8afd1c1a00d6eb0d5a26eabe
camillazvincent <camillazvincent@gmail.com>
2023-04-19 12:52:52 -0400

    minor edits

--- references.php
+++ references.php
@@ -6,6 +6,8 @@
 
     <!-- External stylesheets -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" type="text/css" href="css/styles.css">
 
     <!-- favicon -->
