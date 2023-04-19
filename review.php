b73f5bc22e17266f1d9b95a0787e03a0fbd9e696
Sebastian Smiley <46581584+sebastianswms@users.noreply.github.com>
2023-04-19 18:14:31 -0400

    Update review.php
    Fix typo

--- review.php
+++ review.php
@@ -2,7 +2,7 @@
 <html class="api">
 <head>
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API review</title>
+    <title>API Review</title>
 
     <!-- External stylesheets -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
