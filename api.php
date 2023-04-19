abc10baf2195c7bc5d71d8fa3e3482390b2946aa
camillazvincent <camillazvincent@gmail.com>
2023-04-19 12:46:37 -0400

    Bootstrap the navbar

--- api.php
+++ api.php
@@ -14,26 +14,41 @@
 </head>
 
 <body class="api">
-<nav class="navbar navbar-expand-lg navbar-dark">
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-logo">
-            <a href="index.html">
-              <img src="/images/logo.png" width="100" height="100" alt="Logo">
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
-        </li> 
-        <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="references.php">References</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="api.php">Video Search API</a>
-        </li>
-    </ul>
+<nav class="navbar navbar-expand-sm navbar-dark">
+        <a
+            href="index.html"
+            class="nav-logo mb-0">
+              <img src="/images/logo.png"
+              width="100" height="100" />
+        </a>
+        <button
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        class="navbar-toggler"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item" id="item1">
+               <a class="nav-link" href="index.html"
+               >Home</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="about.php"
+               >About</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="references.php">References</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="api.php">Video Search API</a>
+            </li>
+          </ul>
 </nav>
 <main class="container h-100 w-100">
     <div class="row h-100 justify-content-center align-items-center">
